fdda84eb8440cfd0edfcaf96d1a99f903c86e2fb
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2019-11-18 00:07:04 -0500

    ruta cmbiada

--- src/uri.ts
+++ src/uri.ts
@@ -1,2 +1,2 @@
-// export const uri = `https://gestorappbackend.huamanbellidoj.now.sh`;
-export const uri = 'http://localhost:3005'
+export const uri = `https://gestorappbackend.huamanbellidoj.now.sh`;
+// export const uri = 'http://localhost:3005'
